71418ec1727f0bd9f978b1508fca5ec8611575b5
Sharif-Mahmud <sharif230@gmail.com>
2024-03-13 00:07:33 +0600

    added male or female check

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const name = document.querySelector(".input_box input[placeholder='Name']");
-const birth_year = document.querySelector(
-  ".input_box input[placeholder='Birth Year']"
-);
+const birth_year = document.querySelector(".input_box input[placeholder='Birth Year']");
 const submit = document.querySelector(".input_box input[type='submit']");
 const message = document.querySelector(".input_box h1");
+
+const selectElement = document.getElementById("mySelect");
+
+
+
+
+
+
+
+
+
 
 submit.addEventListener("click", () => {
   if (!name.value || !birth_year.value) {
@@ -11,14 +20,37 @@ submit.addEventListener("click", () => {
   } else {
     let age = new Date().getFullYear() - Number(birth_year.value);
 
-    if (age >= 21) {
-      message.innerHTML = `
+
+    let selectedOption = selectElement.value;
+
+    if (selectedOption == "Male") {
+          if (age >= 21) {
+            message.innerHTML = `
                 ${name.value}, you can go ahead for marry, you are adult.
                 <img src="https://www.gifimages.pics/images/quotes/english/general/gif-for-confetti-multi-colored-52650-166764.gif"/>`;
-    } else {
-      message.innerHTML = `
+          } else {
+            message.innerHTML = `
                 ${name.value}, Sorry you can't do it now, you are not adult.
                 <img src="https://media1.tenor.com/m/N6hA2Al9eM4AAAAC/byuntear-baby-cry.gif"/>`;
+          }
+    } else {
+          if (age >= 18) {
+            message.innerHTML = `
+                ${name.value}, you can go ahead for marry, you are adult.
+                <img src="https://www.gifimages.pics/images/quotes/english/general/gif-for-confetti-multi-colored-52650-166764.gif"/>`;
+          } else {
+            message.innerHTML = `
+                ${name.value}, Sorry you can't do it now, you are not adult.
+                <img src="https://media1.tenor.com/m/N6hA2Al9eM4AAAAC/byuntear-baby-cry.gif"/>`;
+          }
     }
+
+
+
+
+
+
+
+
   }
 });
